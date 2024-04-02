d4db0144d96a66af74d6ac0b3d6d3d418bbae186
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-04-02 21:41:55 +0700

    rename function and check bad request

--- internal/controller/http/v1/proxyImage/proxyImage.go
+++ internal/controller/http/v1/proxyImage/proxyImage.go
@@ -11,15 +11,20 @@ import (
 func InitProxyImageHandler(router *mux.Router) {
 	log.Println("Init proxy image handler")
 
-	router.HandleFunc("/proxy", proxyImage).Methods(http.MethodGet)
+	router.HandleFunc("/proxy", getProxedImage).Methods(http.MethodGet)
 }
 
-func proxyImage(w http.ResponseWriter, r *http.Request) {
+func getProxedImage(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query().Get("url")
 	referer := r.URL.Query().Get("referer")
 	authority := r.URL.Query().Get("authority")
 
-	image := utils.GetProxyImage(url, referer, authority)
+	image, err := utils.FetchImage(url, referer, authority)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
