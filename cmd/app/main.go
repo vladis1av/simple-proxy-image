b8c48fcf58e212180a78e3530458f9a9a3dc0693
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-04-02 22:55:18 +0700

    add config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"simple-proxy-images/internal/app"
+	"simple-proxy-images/internal/config"
+	"simple-proxy-images/internal/config/env"
 )
+
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
+}
 
 func main() {
 	log.Println("Init app")
 
-	application := app.NewApp(":3000")
+	flag.Parse()
+
+	log.Println("Load config")
+	err := config.Load(configPath)
+	if err != nil {
+		log.Fatal("failed to load config: ", err)
+	}
+
+	httpConfig, err := env.NewHTTPConfig()
+	if err != nil {
+		log.Fatal("failed to get http config: ", err)
+	}
+
+	application := app.NewApp(httpConfig.Address())
 
 	application.Run()
 }
